98fce954485c82e7b947cd882d80e42d1c6cf6f0
Carlos Indriago <carlosindriagorizales@gmail.com>
2026-01-29 00:54:19 -0500

    Meta: Update plugin author to Carlos Indriago

--- spectrus-guard.php
+++ spectrus-guard.php
@@ -6,7 +6,7 @@
  * Version:           1.0.0
  * Requires at least: 5.8
  * Requires PHP:      7.4
- * Author:            Carlos Developer
+ * Author:            Carlos Indriago
  * Author URI:        https://yoursite.com
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
